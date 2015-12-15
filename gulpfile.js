1ecb6cc07379de9d1c61af859c7b6b096e9103f5
Xia Yan <xiayan@Xias-MacBook-Air.local>
2015-12-15 18:42:31 +0100

    Installed gulp and gutil

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/**
+ * Created by xiayan on 15/12/2015.
+ */
+var gulp = require ('gulp');
+    gutil = require ('gulp-util');
+
+gulp.task('log', function () {
+    gutil.log('Workflows are awesome');
+})
